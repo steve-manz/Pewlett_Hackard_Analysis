a21927b194ab4d57b61c7198f6e5a4257cac46a5
steve-manz <59142513+steve-manz@users.noreply.github.com>
2020-05-16 15:02:02 -0500

    First commit

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -48,7 +48,7 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 
 CREATE TABLE titles (
   emp_no INT NOT NULL,
-  title varchar NOT NULL,
+  title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
